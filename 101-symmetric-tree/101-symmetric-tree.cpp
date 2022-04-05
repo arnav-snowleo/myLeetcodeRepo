eef0591162a951dd6d8e85e8964ac57560c98f84
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-04-05 14:36:35 +0530

    Time: 3 ms (89.91%), Space: 16.3 MB (73.74%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -12,25 +12,20 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        
-        if(!root) return true;
-        return isSymmetricHelper(root->left , root->right);
+
+        return (!root) || isSymmetricHelper(root->left , root->right);
         
     }
     
-    bool isSymmetricHelper(TreeNode* left, TreeNode* right){
+    bool isSymmetricHelper(TreeNode* root1, TreeNode* root2){
         
         //BASE CASES
-        if(!left || !right) return left == right;
-        if(left->val != right->val) return false;
+        if(!root1 || !root2) return root1 == root2;
+        if(root1->val != root2->val) return false;
         
-        //LEFT SUBTREE
-        //preorder traversal
-        bool  l = isSymmetricHelper(left->left , right->right);
-        
-        //RIGHT SUBTREE
-        //reverse pre-order traversal
-        bool  r = isSymmetricHelper(left->right , right->left);       
+        //preorder traversal simultaneously
+        bool  l = isSymmetricHelper(root1->left , root2->right);
+        bool  r = isSymmetricHelper(root1->right , root2->left);       
         
         //JOIN
         return l && r;
