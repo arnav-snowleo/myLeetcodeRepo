9eeea822a036fabd877564edf6254eb8c3329818
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-04-10 23:12:59 +0530

    Time: 25 ms (21.68%), Space: 15.9 MB (52.96%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        
+        int ceilVal = ceil(nums.size()/3);         
+        
+        // unordered_map<int.int> mp;
+        map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;     
+            
+        }
+        
+        vector<int> ans;
+        for(auto it : mp){
+            
+            if(it.second > ceilVal){
+                ans.push_back(it.first);
+            }
+        }
+        return ans;       
+        
+    }
+};
