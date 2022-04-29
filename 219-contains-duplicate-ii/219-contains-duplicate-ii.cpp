5752f95e5554fd84590e1341dfb7a929f4e287d0
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-04-29 16:24:43 +0530

    Time: 271 ms (36.90%), Space: 78 MB (27.54%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        bool ans = false;
+        
+//         for(int i=0;i<nums.size()-1;i++){
+            
+            
+//             for(int j = i+1;j<nums.size();j++){
+                
+                
+//                 if( nums[i] == nums[j] && abs(i-j) <=k){
+//                     ans = true;
+//                     break;
+//                 }
+//             }
+//         }
+        
+        unordered_map<int,int> mp;
+        
+        for(int i=0;i<nums.size() ; i++){            
+            
+            // perform check at each index
+            
+            if( mp.find(nums[i]) !=  mp.end() ) {
+                
+                //then it has occured earlier
+                //go for checking 
+                
+                if( abs(i- mp[nums[i]] ) <= k ){
+                    ans = true;
+                }
+            }
+            
+            
+            // STORING THE element as key and the index as value in the map
+            mp[nums[i]] = i; 
+              
+            
+        }
+        
+        return ans;
+    }
+};
