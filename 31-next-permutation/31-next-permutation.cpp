374daca534a0d5b0bbe9acfe27545097ce4a662c
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-04 21:57:47 +0530

    Time: 7 ms (51.52%), Space: 11.9 MB (99.91%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        //using inbuilt function
+        next_permutation(nums.begin() , nums.end());
+        
+    }
+};
