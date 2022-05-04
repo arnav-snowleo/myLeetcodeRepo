9d6cd95085a0efec538b1f99bc4dbb4898fac22a
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-04 18:17:11 +0530

    Time: 184 ms (7.74%), Space: 44.7 MB (11.31%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -2,19 +2,43 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         
+        //USE MAP
+        
+        unordered_map<int,int> mp;
         vector<int> ans;
         int n = nums.size();
         
-        sort(nums.begin() , nums.end());
-        
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             
-            if( (nums[i]^nums[i+1]) ==0 ){
-                ans.push_back(nums[i]);
-            }
-            
+            mp[nums[i]]++;
         }
-        return ans;
+        
+        for(auto it: mp){
+            
+            if(it.second>1){
+                ans.push_back(it.first);
+            }
+        }       
+        return ans;       
+        
+        
+        
+        // SORT
+        // XOR
+        
+//         vector<int> ans;
+//         int n = nums.size();
+        
+//         sort(nums.begin() , nums.end());
+        
+//         for(int i=0;i<n-1;i++){
+            
+//             if( (nums[i]^nums[i+1]) ==0 ){
+//                 ans.push_back(nums[i]);
+//             }
+            
+//         }
+//         return ans;
         
     }
 };
