933505f03810f57ba17866e08db97f8f2fc0e277
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-05 22:54:00 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (25.63%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -4,25 +4,19 @@ public:
         
         //COUNT AND SORT USING EXTRA VECTOR SPACE
         
+        vector<int> ans; 
         map<int, int> mp;        
-        int n = nums.size();
+        int n = nums.size(); 
         
         for(int i=0;i<n;i++){            
             mp[nums[i]]++;
-        }
-        
-        vector<int> ans;        
-        for(int i=0;i<mp[0];i++){
-            ans.push_back(0);
-        }
-        for(int i=0;i<mp[1];i++){
-            ans.push_back(1);
-        }
-        for(int i=0;i<mp[2];i++){
-            ans.push_back(2);
-        }     
-        
-        nums= ans;
-        
+        }      
+     
+        for(int j=0;j<mp.size();j++){
+            for(int i=0;i<mp[j];i++){
+                ans.push_back(j);
+            }          
+        }        
+        nums= ans;        
     }
 };
