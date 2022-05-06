d52039c639f3c666a419fca1cb39c9fb6573ef37
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-06 18:56:31 +0530

    Time: 17 ms (5.48%), Space: 12.1 MB (75.41%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,8 +2,61 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
-        //using inbuilt function
-        next_permutation(nums.begin() , nums.end());
+        // without using inbuilt function
+        
+        // four steps
+        
+        // STEP 1 .
+        //  from back iterate and stop at breaking point ( where order broke )
+                     // order not broken thoughout is possible
+                     // just reverse the i/p in that case
+        // O(n)
+        
+        // STEP 2. 
+        // find smallest greater element to the element at breaking point index
+        // O(n)
+        
+        // STEP 3.
+        // swap both
+        
+        // STEP 4.
+        // reverse from back till the breaking point index
+        // O(n)
+        
+        int n = nums.size() , k ,l;
+        
+        for(k=n-2;k>=0;k--){
+            
+            if(nums[k] < nums[k+1]){
+               break;              
+            }
+            
+        }       
+        if(k<0){  // iterated once, no breaking point found            
+            reverse(nums.begin() , nums.end());
+        }
+        
+        else{
+        //STEP 2. find the next(closest) greatest element to that of element at index k
+        for(l=n-1;l>k;l--){
+            if(nums[l] > nums[k]){
+                //we got l also
+                break;
+            }
+        }
+        
+        //STEP 3.
+        swap(nums[k] , nums[l]);
+        
+        //STEP 4.
+        // reverse from k+1 to last
+        reverse(nums.begin()+k+1  , nums.end());
+
+        }  
+        
+        
+        // using inbuilt function
+        // next_permutation(nums.begin() , nums.end());
         
     }
 };
