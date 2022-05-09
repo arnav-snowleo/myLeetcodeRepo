991cb19f9f4ba6846d32b40374db3909895de42e
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-09 19:52:03 +0530

    Time: 3 ms (78.15%), Space: 7.6 MB (78.82%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,15 +1,48 @@
 class Solution {
-public:
-    vector<vector<int>> permute(vector<int>& nums) {        
+private:
+    
+    void allPermutations(int size, vector<int> &ds,int freq[] , vector<int> &nums, vector<vector<int>> &ans ){        
         
-        vector<vector<int>> ans;
-        
-        sort(nums.begin() , nums.end());
-        do {
-            ans.push_back(nums);
+        // Base case        
+        if(ds.size() == size){
+            ans.push_back(ds);
+            return;
         }
-        while( next_permutation( nums.begin() , nums.end() ) );
         
+        for(int i =0;i<size;i++){
+            
+            if(!freq[i]){                               // can pick and add if freq != 1
+                
+                ds.push_back(nums[i]);                          // pick
+                freq[i] = 1;                                    // mark
+                allPermutations(size, ds, freq, nums, ans);
+                
+                // go up
+                
+                freq[i] = 0;                                    // unmark
+                ds.pop_back();                                  // remove              
+                
+            }
+        } 
+        
+    }    
+    
+public:
+    vector<vector<int>> permute(vector<int>& nums) {   
+        
+        // USING RECURSION 
+        // approach 1- uses extra space for <ds, freq array> 
+        // freq array is used to flag indices, with 0 or 1
+        
+        vector<int> ds;
+        vector<vector<int>> ans;
+        int n = nums.size();        
+        
+        int freq[n];        
+        for(int i=0;i<n;i++) freq[i] = 0;
+        
+        allPermutations(n,ds,freq ,nums,ans);      
+
         return ans;
     }
 };
