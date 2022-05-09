71da4a5d39de871e14ad078b38fdbb6e989dc9e2
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-09 20:20:28 +0530

    Time: 3 ms (78.15%), Space: 7.7 MB (55.88%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,48 +1,31 @@
 class Solution {
 private:
-    
-    void allPermutations(int size, vector<int> &ds,int freq[] , vector<int> &nums, vector<vector<int>> &ans ){        
+    void recursivePermute(int index , int size, vector<int> &nums, vector<vector<int>> &ans){
         
-        // Base case        
-        if(ds.size() == size){
-            ans.push_back(ds);
+        // Recursive solution
+        // SWAPPING 
+        // extra space for ds, freq[] not used
+        
+        if(index == size){
+            ans.push_back(nums);
             return;
         }
         
-        for(int i =0;i<size;i++){
-            
-            if(!freq[i]){                               // can pick and add if freq != 1
-                
-                ds.push_back(nums[i]);                          // pick
-                freq[i] = 1;                                    // mark
-                allPermutations(size, ds, freq, nums, ans);
-                
-                // go up
-                
-                freq[i] = 0;                                    // unmark
-                ds.pop_back();                                  // remove              
-                
-            }
-        } 
+        for(int i=index;i<size;i++){
+            swap(nums[index], nums[i]);                   // swap
+            recursivePermute(index+1 , size, nums, ans);  // recur
+            swap(nums[index], nums[i]);                   // re-swap on way back
+        }        
+             
         
-    }    
-    
+    }
 public:
-    vector<vector<int>> permute(vector<int>& nums) {   
+    vector<vector<int>> permute(vector<int>& nums) {
         
-        // USING RECURSION 
-        // approach 1- uses extra space for <ds, freq array> 
-        // freq array is used to flag indices, with 0 or 1
-        
-        vector<int> ds;
+        int n = nums.size();
         vector<vector<int>> ans;
-        int n = nums.size();        
-        
-        int freq[n];        
-        for(int i=0;i<n;i++) freq[i] = 0;
-        
-        allPermutations(n,ds,freq ,nums,ans);      
-
+        recursivePermute(0,n,nums,ans);
         return ans;
+        
     }
 };
