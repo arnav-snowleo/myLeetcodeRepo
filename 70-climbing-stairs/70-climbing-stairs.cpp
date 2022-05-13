fd965b31db6c3d1ceaa3840e7007afcb21af1b95
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-13 22:46:38 +0530

    Time: 3 ms (30.79%), Space: 5.9 MB (56.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,32 +1,20 @@
 class Solution {
-private:
-//     int fun(int n , vector<int> &dp){
-//         if(n <2) return 1;
-        
-//         //MEMOIZE : STEP 2
-//         if(dp[n] != -1) return dp[n];
-        
-//         int l = fun(n-1 , dp);
-//         int r = fun(n-2 , dp);
-        
-//         //MEMOIZE : STEP 3. store while returning
-//         return dp[n] = l+r;
-        
-//     }
 public:
     int climbStairs(int n) {
         
-        //TABULATION SOLUTION
-        int dp[n+1];
+        //TABULATION SOLUTION - SPACE OPTIMIZED
         
-        dp[0] = 1;
-        dp[1] = 1;
-        
+        int prev2 = 1, prev =1;
+
         for(int i = 2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev2 + prev ;
+            
+            prev2 = prev;
+            prev = curr;
+            
         }
         
-        return dp[n];
+        return prev;
         
     }
 };
