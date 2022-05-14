35b21f529291f4ad6b786a355509398f9a940212
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-14 17:43:54 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (95.10%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -8,21 +8,24 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         
-        // TABULATION APPROACH - bottom up
+        // TABULATION APPROACH - bottom up - space optimized -- O(1) space
        
         int n = nums.size();
-        int dp[n+1];        
-        dp[0] = nums[0];       
+        int prev = nums[0];     
+        int prev2 = 0;
         
         for(int i=1;i<n;i++){
             
             int pick = nums[i] ;            
-            if(i>1) pick += dp[i-2];
-            int not_pick = 0+ dp[i-1];
+            if(i>1) pick += prev2;
+            int not_pick = 0 + prev;
             
-            dp[i] = max(pick, not_pick);
+            int curr = max(pick, not_pick);
+            
+            prev2 = prev;
+            prev = curr;
         }
         
-        return dp[n-1];        
+        return prev;        
     }
 };
