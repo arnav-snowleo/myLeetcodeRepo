45ea6a25697308d167b830d3f9c17557982a7964
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-19 17:33:30 +0530

    Time: 42 ms (5.14%), Space: 12.6 MB (76.70%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,35 +1,58 @@
+// AMAZON OA
+
+// solve with better appraoch (better TC, SC)
+
+        //largest, maximum , frequent -> MIN HEAP O(nlogk)
+        //                            -> MAX HEAP can also use, but O(nlogn)
+
+struct comparision
+{
+    bool operator()(const pair<int,string> &a,const pair<int,string> &b)
+    {
+        //if count of both equal, return lexicographically smaller
+        if(a.first==b.first)
+           return a.second<b.second;
+        
+        return a.first>b.first; //else greater (as convention in min_heap)
+    }
+};
+
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        
-        // using map automatically will put in lexicographical order
-        
-        map<string, int> mp;                
-        for(int i=0;i<words.size();i++){
-            mp[words[i]]++;
-        }
-        
-        vector<string> ans;    
-        
-        int maxi = 0;
-        for(auto it : mp){
-            maxi = max(maxi, it.second);
-        }
-        
-        int count = 0;
 
-        for(int j =maxi;j>0 ;j--){
-
-            for(auto it: mp){
-                    
-                if(it.second == j){
-                    count++;
-                    if(count <=k) ans.push_back(it.first);
-                }
-            }                
+        //STORE IN MAP for count 
+        unordered_map<string , int> mp;     //insertion into unordered_map is O(1)    
+        for(auto it: words){            
+            mp[it]++;
         }
-        return ans;        
         
+        priority_queue< pair<int, string> , vector<pair<int, string>> , comparision > min_heap;
+                
+        for(auto it: mp){
+            
+            //mp second is count , mp first is string
+            min_heap.push({it.second , it.first}); 
+            
+            if(min_heap.size() > k){
+                min_heap.pop(); 
+                // pops the pair which is not largest (among the existing and incoming)
+            }
+        }
+        // min heap is pair<int, strin>
+        // $$## min heap stores {1, stringa} , {2,stringb} {3,stringc} ..
+        
+        vector<string> ans;
+        while(k--){
+            
+            //min_heap first is count , min_heap second is string
+            ans.push_back(min_heap.top().second);
+            min_heap.pop();
+        }
+        
+        // $$## reverse as we want the strings having max count to be present first
+        reverse(ans.begin(), ans.end());
+        return ans;
         
     }
 };
