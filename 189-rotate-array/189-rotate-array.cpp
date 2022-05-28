f6c0dd4bf27056d992c7526c9a85687b79dc3b5a
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-29 01:39:14 +0530

    Time: 39 ms (57.63%), Space: 25.1 MB (53.25%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -2,42 +2,22 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         
-        //BRUTE - TC: O(n) , SC: O(n)
+        // TC: O(n) , SC: O(1)
         
-        int n = nums.size();
-        if(k>n) k = k%n;
-        vector<int> temp;
+        // without using extra space
+        // OBSERVE PATTERN - reverse thrice
         
-        for(int i=n-k;i<n;i++){
-            temp.emplace_back(nums[i]);
-        }
-        for(int i=0;i<n-k;i++){
-            temp.emplace_back(nums[i]);
-        }        
-        nums = temp;         
+        //reverse complete array
+        
+        // now reverse from start till k
+        // now reverse from k to end
+        
+        reverse(nums.begin(),nums.end());
+        
+        k %= nums.size();
+        
+        reverse(nums.begin() , nums.begin()+k);
+        reverse(nums.begin()+k , nums.end());
+        
     }
 };
-
-
-/*
-int n = nums.size();
-        
-        vector<int> temp;
-        temp = nums;
-
-        int x = nums[n-1];
-        
-        if(k>n) k = k%n;
-        
-        for(int i=n-1;i>=0;i--){  
-            
-            if((i+k) < n){
-                cout<<"hua";
-                nums[i+k] = temp[i]; 
-            }
-            else{
-                cout<<"la";
-                // nums[i-k] = nums[i];  
-            }
-        }   
-*/
