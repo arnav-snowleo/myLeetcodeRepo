3094c593a844d86cb09be4eca92cabf55e29c6ca
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-05-31 15:53:27 +0530

    Time: 8 ms (99.31%), Space: 13.7 MB (72.53%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,3 +1,6 @@
+#define ss second
+#define ff first
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -5,32 +8,28 @@ public:
         unordered_map<int,int> mp;        
         for(auto it: nums){
             mp[it]++;
-        }
-        
-        //mp->second has count, mp->first has the element
-        
-        //frequent -> means maximum -> use min_heap (priority queue)        
-        priority_queue< pair<int,int>, vector<pair<int, int>> , greater< pair<int,int> >> min_heap;
-         
+        }     
+        priority_queue< pair<int,int>, vector<pair<int, int>> , greater< pair<int,int> >> min_heap;         
         
         //we sort on basis count, so that should be key of min_heap
         
-        for(auto it: mp){
+        for(auto it: mp){         
             
-            min_heap.push({it.second , it.first});
-            
-            if(min_heap.size() > k){
-                min_heap.pop();
-            }
+            min_heap.push({it.ss , it.ff});            
+            if(min_heap.size() > k)  min_heap.pop();            
         }
         
-        vector<int> ans;   
+        vector<int> ans;
         
         while(k--){
-            ans.push_back(min_heap.top().second);
+            ans.push_back(min_heap.top().ss);
             min_heap.pop();
         }
 
         return ans;       
     }
 };
+
+        
+//mp->second has count, mp->first has the element
+//frequent -> means maximum -> use min_heap (priority queue)   
