76218a7e5180d56ec91b33100e2cbb637618cfba
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-02 00:58:58 +0530

    Time: 114 ms (47.08%), Space: 41.3 MB (40.54%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -0,0 +1,21 @@
+// PREFIX sum, HASHMAP
+// sum is key in map
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        int count =0,sum=0, n = nums.size();
+        unordered_map<int,int> mp;
+        
+        for(int i=0;i<n;i++){
+            sum += nums[i];          
+            
+            if(sum==k) count++;
+            if(mp.find(sum - k) != mp.end()) count+= mp[sum-k]; 
+            
+            mp[sum]++;           
+        }
+        return count;        
+    }
+};
