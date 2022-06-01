414d5ef35d7fb44c417bb1eeeff23dd44d229dfd
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-01 16:39:38 +0530

    Time: 17 ms (37.02%), Space: 8 MB (58.25%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,28 +9,19 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        
-        ListNode* temp = head;
-        if(temp==NULL || temp->next == NULL){
-            //zero node or only one node
-            return false;
-        }
-        
+
         ListNode* slow = head;
         ListNode* fast = head;
         
-        bool hasCycle = false;           
+        if(slow==NULL || slow->next == NULL)  return false;
         
         while(fast && fast->next){
             
             slow = slow->next;
             fast = fast->next->next;
             
-            if(slow == fast){
-                hasCycle = true;
-                break;
-            }
+            if(slow == fast)   return true;
         }
-        return hasCycle;
+        return false;
     }
 };
