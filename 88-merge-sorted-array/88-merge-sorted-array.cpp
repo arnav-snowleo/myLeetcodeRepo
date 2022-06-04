683356d2a70395609dc7324cbff3e5839e24c846
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-05 00:50:02 +0530

    Time: 12 ms (6.59%), Space: 9.1 MB (31.00%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,16 +1,35 @@
-// SPACE OPTIMIZED: WITHOUT USING EXTRA SPACE
-// TC: O((n+m)logn) + O(n)
+// SPACE OPTIMIZED: WITHOUT USING EXTRA SPACE  
+// TC: 
 
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
-        if(m==0 && n!=0)  nums1 = nums2;   
+//         if(m==0 && n!=0)  nums1 = nums2;   
         
-        for(int i=0;i<n;i++){                 // O(n)
-            nums1[m+i] = nums2[i]; 
+//         for(int i=0;i<n;i++){                 // O(n)
+//             nums1[m+i] = nums2[i]; 
+//         }
+//         sort(nums1.begin() , nums1.end());    // O((n+m)logn) 
+        
+        int i=0,j=0;
+        while(i<m && j<n){                           //O(m+n)  
+            
+            if(nums1[i] <= nums2[j]){
+                i++;
+            }else{
+                swap( nums1[i] , nums2[j]);
+                sort(nums2.begin() , nums2.end());   //O(nlogn)
+            }
         }
-        sort(nums1.begin() , nums1.end());    // O((n+m)logn) 
+        
+        for(int i=0;i<n;i++){                        //O(n)
+            nums1[m+i] = nums2[i];
+        }
+        
+        
+        //worse TC:
+        //O(n) + O((m+n) * nlogn )
     }
 };
  
