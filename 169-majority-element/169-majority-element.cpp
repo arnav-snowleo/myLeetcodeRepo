001aa9486d859d2199d388d6279f13f684128577
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-05 03:13:04 +0530

    Time: 27 ms (51.38%), Space: 19.5 MB (59.47%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,12 +1,30 @@
+//  MOST OPTIMIZED APPROACH   - MOORE'S VOTING ALGO
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
+        int count = 0;
+        int candidate = 0;        
+        
+        for(auto it: nums){
+            
+            if(count ==0) candidate = it;
+            if(candidate == it) count++;
+            if(candidate != it) count--;
+        }      
+        
+        return candidate;       
+    }
+};
+
+
+
+// BRUE APPROACH, USE HASHMAPS
+/*
         int ceilVal = ceil(nums.size()/2);        
         int ans = 0;        
-        
-        // map<int, int> mp;
-        
+
         unordered_map<int,int> mp; 
         
         for (int i =0;i<nums.size();i++){
@@ -16,10 +34,8 @@ public:
         for(auto it : mp){
             
             if(it.second > ceilVal){
-                ans = it.first;
+                return it.first;
             }
         }        
         return ans;   
-        
-    }
-};
+*/
