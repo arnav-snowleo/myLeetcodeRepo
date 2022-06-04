24da36f4415d2d6a5be289512aa505812d8ecbe3
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-05 03:57:46 +0530

    Time: 4 ms (30.97%), Space: 6 MB (44.74%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -6,14 +6,15 @@ private:
         
         if(n==0) return 1.0;
         
-        if(n%2 ==0) return helper(x*x , n/2);
-        else return x * helper(x, n-1);
+        if(n%2) return x * helper(x, n-1);          // n%2 ==1
+        else return helper(x*x , n/2);              // n%2 ==0
         
     }
 public:
     double myPow(double x, int n) {
         
-        
+        // n == -2147483648 , while converting this particular val to +ve, will overflow
+        // as +ve int max val can be 2147483647
         // n can be large negative , can cause overflow, so take long long
         long long int z = abs(n);                        // taking int only , gives Runtime error
         double ans = 1.0 ;
