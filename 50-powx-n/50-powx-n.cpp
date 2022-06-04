2ad49c4d9ca02ff2758160a03d794dec0805be8c
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-05 03:47:43 +0530

    Time: 4 ms (30.97%), Space: 5.7 MB (99.96%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,25 @@
+//RECURSIVE : O(logn)
+
+class Solution {
+private:
+    double helper(double x, long long int n){
+        
+        if(n==0) return 1.0;
+        
+        if(n%2 ==0) return helper(x*x , n/2);
+        else return x * helper(x, n-1);
+        
+    }
+public:
+    double myPow(double x, int n) {
+        
+        // n can be large negative , can cause overflow, so take long long
+        long long int z = abs(n);
+        double ans = 1.0 ;
+        ans = helper(x,z);
+        
+        if(n<0)
+            return 1/ans;
+        return ans;
+    }
+};
