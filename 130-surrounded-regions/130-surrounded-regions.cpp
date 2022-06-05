fa62466faf6d6964b41e746b5258c030cadb9dae
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-06 03:33:05 +0530

    Time: 17 ms (58.48%), Space: 9.9 MB (79.25%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -23,32 +23,32 @@ public:
         int m = board.size();
         int n = board[0].size();
         
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){    
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){    
                 
-//                 //for boundary 'O's and connected 'O's to them, dfs, and mark '#'
+                //for boundary 'O's and connected 'O's to them, dfs, and mark '#'
                 
-//                 if((j==0 || j==n-1 || i==0 || i==m-1) && board[i][j]=='O'){                
-//                     dfs(i,j, m, n , board);
-//                 }
-//             }
-//         }       
+                if((j==0 || j==n-1 || i==0 || i==m-1) && board[i][j]=='O'){                
+                    dfs(i,j, m, n , board);
+                }
+            }
+        }       
         
-        //first row, last row
-        for(int j = 0; j<n ; j++ ){
-            if(board[0][j] == 'O')
-                 dfs(0,j, m, n , board);
-            if(board[m- 1][j] == 'O')
-                 dfs(m-1,j, m, n , board);
-        }
+//         //first row, last row
+//         for(int j = 0; j<n ; j++ ){
+//             if(board[0][j] == 'O')
+//                  dfs(0,j, m, n , board);
+//             if(board[m- 1][j] == 'O')
+//                  dfs(m-1,j, m, n , board);
+//         }
         
-        //first col, last col
-        for(int i = 0; i < m; i++){
-            if(board[i][0] == 'O')
-                dfs(i,0, m, n , board);
-            if(board[i][n - 1] == 'O')
-                dfs(i,n-1, m, n , board);
-        }
+//         //first col, last col
+//         for(int i = 0; i < m; i++){
+//             if(board[i][0] == 'O')
+//                 dfs(i,0, m, n , board);
+//             if(board[i][n - 1] == 'O')
+//                 dfs(i,n-1, m, n , board);
+//         }
         
         //after DFS traversal
         for(int i=0;i<m;i++){
