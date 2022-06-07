e3b343634b083a26c38afbfd35ce7b96abb01ff5
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-08 01:51:02 +0530

    Time: 16 ms (87.91%), Space: 20.1 MB (99.31%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -2,31 +2,22 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         if(n==0) return true;
+        
         int m = flowerbed.size();
         if(m==1 && flowerbed[0] != n) return true;
-        
-        if(flowerbed[0] ==0 && flowerbed[1]==0) {
-            flowerbed[0] = 1;
-            n--;
-            if(n==0) return true;
-        }        
-        
-        for(int i=1;i<m-1;i++){
-            if(flowerbed[i] == 0 && flowerbed[i-1]==0 && flowerbed[i+1]==0) {
+
+        for(int i=0;i<m;i++){
+            
+            bool left = (i-1>=0) ? ( flowerbed[i-1]==0 ? true : false ) : true ;
+            bool right = (i+1 < m) ? ( flowerbed[i+1]==0 ? true : false ) : true ;
+            
+            if(flowerbed[i] == 0 && left && right) {
                 flowerbed[i] = 1;
                 n--;
-                if(n==0) return true;
-                
-            }
-            
-        }
+                if(n==0) return true;                
+            }            
+        }         
         
-        if(flowerbed[m-1] ==0 && flowerbed[m-2]==0) {
-            flowerbed[m-1] = 1;
-            n--;
-            if(n==0) return true;
-        }    
-        
-        return !(n!=0) ;
+        return n<=0;
     }
 };
