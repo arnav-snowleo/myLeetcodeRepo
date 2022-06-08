a662613a97e020bf8ccfd151368128672e85afa3
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-09 02:24:43 +0530

    Time: 9 ms (49.46%), Space: 8.5 MB (84.49%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,54 @@
+//USING BFS
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+
+public:
+    unordered_map<Node*, Node*> mp;
+    Node* cloneGraph(Node* node) {
+        
+        if(!node) return NULL;
+        
+        Node* temp = new Node(node->val, {});
+        mp[node] = temp;
+        
+        queue<Node*> q;       
+        q.push(node);
+        
+        while(!q.empty()){
+            
+            auto curr = q.front();
+            q.pop();
+            
+            for(auto adj : curr->neighbors){
+                
+                if(mp.find(adj) == mp.end()){            // node not present in the map
+                    mp[adj] = new Node(adj->val ,{});    // add to map
+                    q.push(adj);                         // push to queue
+                }
+                mp[curr]->neighbors.push_back(mp[adj]);                
+            }           
+        }
+        return mp[node];
+    }
+};
