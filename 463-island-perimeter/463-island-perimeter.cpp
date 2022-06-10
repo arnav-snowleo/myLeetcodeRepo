040b91c1a197b59bb63f6f3273d09e032734471a
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-11 02:32:30 +0530

    Time: 172 ms (38.45%), Space: 96.4 MB (38.59%) - LeetHub

---
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -0,0 +1,44 @@
+//1 - land
+//0-  water
+//2- already visited land
+
+// wherever i cannot go from particular land cell, i increment the counter
+
+// say , I am at grid[1][1] ->can go everywhere, so don't increase counter at all
+
+class Solution {
+public:
+    int count =0;
+    void dfs(int i, int j, int row, int col, vector<vector<int>>& grid){
+        
+        if(i<0 || i>=row || j<0 || j>=col || grid[i][j] == 0 ) {
+            count++;
+            return;
+        }
+        if(grid[i][j] == 2) return;
+        
+        grid[i][j] = 2;
+        
+        dfs(i+1,j,row, col, grid);
+        dfs(i-1,j,row, col, grid);
+        dfs(i,j+1,row, col, grid);
+        dfs(i,j-1,row, col, grid);
+           
+    }
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        int ans =0;
+        for(int i=0;i<m;i++){
+            
+            for(int j=0;j<n;j++){
+                
+                if(grid[i][j]==1) dfs(i,j,m,n,grid);
+            }
+        }
+        
+        return count;       
+    }
+};
