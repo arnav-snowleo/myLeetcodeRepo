1c43ea6a76e813fa0a78dd4ae0424ebb543ce3fc
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-16 22:55:22 +0530

    Time: 2386 ms (5.01%), Space: 184 MB (5.43%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,40 @@
+// using 3 pointers //sorting
+// i pointer, sort and use left pointer, right pointer
+
+// solution set must not contain duplicate triplets. // USE SET logn time for insertion
+    
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        int n=nums.size();        
+        sort(nums.begin(), nums.end());        
+        vector<vector<int>> ans;
+        set<vector<int>> setvector;
+        if(n < 3)  return ans;
+        
+        for(int i=0;i<n-2;i++){           
+        
+            int j = i+1,k=n-1;
+            int rem_sum = -nums[i];
+            while(j<k){
+                if(nums[j] + nums[k] == rem_sum ){       
+                    setvector.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                }
+                else if(nums[j] + nums[k] < rem_sum){
+                    j++;
+                }else{
+                    k--;
+                }                
+            }            
+        }
+        
+        for(auto it: setvector){
+            ans.push_back(it);
+        }
+        
+        return ans;
+    }
+};
