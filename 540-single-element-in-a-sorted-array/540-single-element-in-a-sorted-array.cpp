253f15910cf745b687ee525341b090d1ac41bbc3
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-17 05:16:03 +0530

    Time: 31 ms (77.44%), Space: 22.4 MB (54.48%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,17 +1,54 @@
 // sorted array , solution must run in O(log n) -- hints binary search
+// OPTIMIZED APPROACH - binary search - O(logn)
 
 
-// BRUTE APPROACH : bit manipulation , O(n)
+// recursive approach 
 
 class Solution {
+private:
+    int helper(vector<int>& nums, int start, int end){
+        
+        if(start>end) return -1;
+        
+        if(start==end){
+            return nums[end];
+        }                 
+            
+        int mid = start + (end-start)/2;
+        int ans = nums[mid];
+            
+        if(nums[mid]==nums[mid+1]){
+            ans = max(helper(nums, start, mid-1),helper(nums, mid+2, end));    
+        }
+        else if(nums[mid]==nums[mid-1]){
+            ans = max(helper(nums, start, mid-2),helper(nums, mid+1, end));       
+        }        
+        
+        return ans;
+    }
+    
 public:
     int singleNonDuplicate(vector<int>& nums) {
         
+        int n = nums.size();
+        return helper(nums,0,n-1);
+    }
+};
+
+
+
+
+
+
+
+/*
+// BRUTE APPROACH : bit manipulation , O(n)
+
         int n = nums.size();
         int x = 0;
         for(int i=0;i<n;i++){
             x ^= nums[i];
         }
         return x;
-    }
-};
+
+*/
