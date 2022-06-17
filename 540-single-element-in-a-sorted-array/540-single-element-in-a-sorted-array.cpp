ec407c5c4bfaec23d94fd06d4fe5c8d258a1cd8b
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-17 05:39:56 +0530

    Time: 34 ms (66.25%), Space: 22.4 MB (9.02%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,39 +1,34 @@
 // sorted array , solution must run in O(log n) -- hints binary search
 // OPTIMIZED APPROACH - binary search - O(logn)
 
+// most optimized approach - binary search - odd even index approach
 
-// recursive approach 
-
-class Solution {
-private:
-    int helper(vector<int>& nums, int start, int end){
-        
-        if(start>end) return -1;
-        
-        if(start==end){
-            return nums[end];
-        }                 
-            
-        int mid = start + (end-start)/2;
-        int ans = nums[mid];
-            
-        if(nums[mid]==nums[mid+1]){
-            ans = max(helper(nums, start, mid-1),helper(nums, mid+2, end));    
-        }
-        else if(nums[mid]==nums[mid-1]){
-            ans = max(helper(nums, start, mid-2),helper(nums, mid+1, end));       
-        }        
-        
-        return ans;
-    }
-    
+class Solution {    
 public:
     int singleNonDuplicate(vector<int>& nums) {
         
-        int n = nums.size();
-        return helper(nums,0,n-1);
+        int n = nums.size(),start=0,end=n-1;
+        
+        while(start<end){
+            
+            int mid = start + (end-start)/2;
+            
+            if(mid%2 == 0){                         //mid at even index
+                if(nums[mid] != nums[mid+1]) end = mid;                   // ele is in left part
+                else start = mid+1;                                       // ele is in right part
+            }
+            else{                                   //mid at odd index
+                if(nums[mid] != nums[mid+1]) start = mid+1;               // ele is in right part
+                else end = mid;                                           // ele is in left part
+            }           
+        }       
+        return nums[start];
     }
 };
+
+
+
+
 
 
 
@@ -51,4 +46,30 @@ public:
         }
         return x;
 
+*/
+
+
+/*
+RECURSIVE APPROACH: modifying the binary seacrh
+// private:
+//     int helper(vector<int>& nums, int start, int end){
+        
+//         if(start>end) return -1;
+        
+//         if(start==end){
+//             return nums[end];
+//         }                 
+            
+        
+//         int ans = nums[mid];
+            
+//         if(nums[mid]==nums[mid+1]){
+//             ans = max(helper(nums, start, mid-1),helper(nums, mid+2, end));    
+//         }
+//         else if(nums[mid]==nums[mid-1]){
+//             ans = max(helper(nums, start, mid-2),helper(nums, mid+1, end));       
+//         }        
+        
+//         return ans;
+//     }
 */
