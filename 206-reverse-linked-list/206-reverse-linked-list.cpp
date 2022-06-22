f6ded56de32a3adef173ae807655e539169f154d
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-23 05:23:01 +0530

    Time: 6 ms (82.15%), Space: 8.4 MB (20.38%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -8,39 +8,68 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// USING STACK
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         
-        // Iterative solution
+        stack<int> st;
+        ListNode* temp = head;
+        ListNode* temp2 = head;
         
-        // three pointers
-        // prevPtr
-        // currPtr
-        // nextPtr
-
-        ListNode *prevPtr = NULL;
-        ListNode *currPtr = head;
-        ListNode *nextPtr;
+        if(!temp) return NULL;
         
-        while(currPtr != NULL){            
+        while(temp){   
             
-            nextPtr = currPtr->next; //initialise nextPtr
-            currPtr->next = prevPtr; //detach
-                        
-            prevPtr = currPtr;       //update prevPtr
-            currPtr = nextPtr;       //update 
-            
-            // the order of execution of the 4 lines of code is important.
-            
-            //nextPtr
-            //current->next
-            
-            //prevPtr
-            //currPtr           
-            
+            st.push(temp->val);
+            temp= temp->next;           
         }
-        return prevPtr;   
         
+        while(temp2){   
+            
+            temp2->val = st.top();
+            st.pop();
+            temp2= temp2->next;           
+        }     
+        
+        return head;        
     }
 };
+
+// // Iterative solution
+        
+//         // three pointers
+//         // prevPtr
+//         // currPtr
+//         // nextPtr
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+
+//         ListNode *prevPtr = NULL;
+//         ListNode *currPtr = head;
+//         ListNode *nextPtr;
+        
+//         while(currPtr != NULL){            
+            
+//             nextPtr = currPtr->next; //initialise nextPtr
+//             currPtr->next = prevPtr; //detach
+                        
+//             prevPtr = currPtr;       //update prevPtr
+//             currPtr = nextPtr;       //update 
+            
+//             // the order of execution of the 4 lines of code is important.
+            
+//             //nextPtr
+//             //current->next
+            
+//             //prevPtr
+//             //currPtr           
+            
+//         }
+//         return prevPtr;   
+        
+//     }
+// };
