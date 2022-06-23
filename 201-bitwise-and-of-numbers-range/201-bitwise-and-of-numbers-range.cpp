d366eb5473c944e59ce258681e0bd9d1fd84fad0
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-23 05:51:26 +0530

    Time: 353 ms (5.13%), Space: 5.9 MB (74.00%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
         
+        if(left==0) return 0;
+        if(left==right) return right;
+        
         int ans;
         for(long long int i=left;i<=right;i++){
             ans &= i;
-            
-            if(ans==0) return 0; //not added earlier
+            if(ans==0) return 0; 
         }
         return ans;
     }
