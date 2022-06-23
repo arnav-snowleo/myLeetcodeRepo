d2af0c7209334f266bf3bf3308b233c2f08df104
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-23 20:30:16 +0530

    Time: 25 ms (25.37%), Space: 18.4 MB (36.14%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,29 +1,60 @@
-// BRUTE: 
+// OPTIMISED:
 
-// USING A SET
+// USING TWO POINTERS
 
-//set stores unique ele, only
-//clear array
-//then put set elements into the array
+// if lptr == rptr, increment rptr
+// if different, increment lptr, update val at lptr 
 
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
-        int n = nums.size();
-        
-        set<int> s;
-        for(int i=0;i<n;i++){
-            
-            s.insert(nums[i]);        
-        }        
+        int n = nums.size(),i=0;
 
-        nums.clear();
-        
-        for(auto it: s){
-           nums.emplace_back(it);
-        }
-        
-        return s.size();        
+        for(int j=0;j<n;j++){
+            
+            if(nums[i]==nums[j]){
+                // j++;
+            }else{
+                i++;
+                nums[i] = nums[j];
+            }                   
+        }        
+        return i+1;        
     }
 };
+
+
+
+
+
+
+// // BRUTE: 
+
+// // USING A SET
+
+// //set stores unique ele, only
+// //clear array
+// //then put set elements into the array
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+
+//         int n = nums.size();
+        
+//         set<int> s;
+//         for(int i=0;i<n;i++){
+            
+//             s.insert(nums[i]);        
+//         }        
+
+//         nums.clear();
+        
+//         for(auto it: s){
+//            nums.emplace_back(it);
+//         }
+        
+//         return s.size();        
+//     }
+// };
