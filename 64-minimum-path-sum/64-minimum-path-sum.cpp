215710b45e1aaa898b6a53d2eff0b0e3081f0eaf
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-29 07:24:10 +0530

    Time: 16 ms (43.09%), Space: 10 MB (51.32%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,9 +1,51 @@
-// TABULATION APPROACH : bottom up
+// TABULATION APPROACH : bottom up, with space OPTIMIZATION
 
 class Solution {
 private:
 
 public:
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        int m = grid.size();
+        int n = grid[0].size();               
+        
+        vector<int> prev(n,0);
+        
+        for(int i=0;i<m;i++){
+            
+            vector<int> curr(n,0);
+            
+            for(int j=0;j<n;j++){
+                
+                if(i==0 && j==0) curr[j] =  grid[i][j]; 
+                else{
+                                    
+                    int down = grid[i][j] , right = grid[i][j];
+                
+                    if(i>0) down += prev[j]; 
+                    else down = 1e5;                     //missed this
+                    
+                    if(j>0) right += curr[j-1]; 
+                    else right = 1e5;                    //missed this
+                
+                    curr[j] = min(down, right);   
+                }             
+            }
+            
+            prev = curr;
+        }       
+        return prev[n-1];        
+    }
+};
+
+
+
+
+
+
+
+// TABULATION WITHOUT SPACE OPTIMIZATION
+/*
     int minPathSum(vector<vector<int>>& grid) {
         
         int m = grid.size();
@@ -32,8 +74,8 @@ public:
         }       
         return dp[m-1][n-1];        
     }
-};
 
+*/
 
 
 
