bf980c2fddba481cd988b627c9a97fa275a095e3
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-29 07:17:42 +0530

    Time: 12 ms (68.70%), Space: 9.6 MB (89.40%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,9 +1,49 @@
+// TABULATION APPROACH : bottom up
+
 class Solution {
 private:
+
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        int m = grid.size();
+        int n = grid[0].size();        
+        
+        int dp[m][n];
+        
+        for(int i=0;i<m;i++){
+            
+            for(int j=0;j<n;j++){
+                
+                if(i==0 && j==0) dp[i][j] =  grid[i][j]; 
+                else{
+                                    
+                    int down = grid[i][j] , right = grid[i][j];
+                
+                    if(i>0) down += dp[i-1][j]; 
+                    else down = 1e5;                     //missed this
+                    
+                    if(j>0) right += dp[i][j-1]; 
+                    else right = 1e5;                    //missed this
+                
+                    dp[i][j] = min(down, right);   
+                }             
+            }        
+        }       
+        return dp[m-1][n-1];        
+    }
+};
+
+
+
+
+
+//memoized approach
+/*
     int helper(int i,int j, vector<vector<int>>& grid, vector<vector<int>>& dp){
         
         //write base cases
-        if(i<0 || j<0) return 10000;
+        if(i<0 || j<0) return 1e5;                //using INT_MAX causes overflow
         if(i==0 && j==0) return grid[0][0];       
         
         if(dp[i][j] != -1) return dp[i][j];
@@ -14,13 +54,4 @@ private:
         return dp[i][j] = min(up , left);
         
     }
-public:
-    int minPathSum(vector<vector<int>>& grid) {
-        
-        int m = grid.size();
-        int n = grid[0].size();        
-        vector<vector<int>> dp(m , vector<int>(n,-1));
-        
-        return helper(m-1,n-1,grid,dp);        
-    }
-};
+*/
