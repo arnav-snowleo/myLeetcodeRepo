21a5eacde5ce52ab8362037461053ed6c8979457
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-30 01:24:00 +0530

    Time: 233 ms (24.05%), Space: 93.4 MB (12.13%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,27 +1,22 @@
+// O(n) time complexity using one pass solution 
+        
+//store the minimum price <till the left of the current index we are at>
+//calculate profit at each index
+//store the profits and update max_profit accordingly
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        // O(n) time complexxity using one pass solution 
-        
-        //store the minimum price and check in loop for profit        
-        //store the profits and update max_profit accordingly
-        
-        int mini = INT_MAX;
-        int n = prices.size();
-        int profit,max_profit = 0;
-        
+        int mini = INT_MAX, max_profit = 0, n = prices.size();
+
         for(int i =0; i<n ;i++){      
             
             mini = min(mini, prices[i]);
             
-            profit = prices[i] - mini;
-            max_profit = max(max_profit , profit);
-            
+            int profit = prices[i] - mini;
+            max_profit = max(max_profit , profit);            
         }   
-
         return max_profit;    
-        
-        //two loops -> O(n*n) solution
     }
 };
