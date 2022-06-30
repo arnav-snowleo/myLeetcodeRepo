23ae266935f1b77baf5272b6372a24f08282c578
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-30 06:19:12 +0530

    Time: 19 ms (15.74%), Space: 15.3 MB (10.58%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,41 +1,84 @@
-// RECURSIVE MEMOIZED SOLUTION
+// TABULATION SOLUTION
 
 class Solution {
-private:
-    int helper(int index, int buy, vector<int> &prices,  vector<vector<int>> &dp){
-        
-        if(index == prices.size()) return 0;
-        
-        if(dp[index][buy] != -1) return dp[index][buy];
-        
-        int profit = 0;
-        if(buy){
-            //can buy
-            
-            int x = -prices[index] + helper(index+1, 0, prices, dp);  //buys          ,can't buy next
-            int y =                + helper(index+1, 1, prices, dp);  //does not buy  ,can buy next
-            
-            profit = max(x,y);
-            
-        }else{
-            //can sell only           
-            
-            int x = prices[index] + helper(index+1, 1, prices, dp);   //sells          ,can buy next
-            int y =               + helper(index+1, 0, prices, dp);  //does not sell  ,can't buy next 
-            
-            profit = max(x,y);
-            
-        }       
-        return dp[index][buy] = profit;
-    }
 public:
     int maxProfit(vector<int>& prices) {
         
-        int size = prices.size();
-        vector<vector<int>> dp(size, vector<int>(2, -1));
-        return helper(0,1, prices , dp);        
+        int n = prices.size();
+        
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        dp[n][0] = 0, dp[n][1] = 0;
+        
+        for(int index=n-1;index>=0;index--){
+            
+            for(int buy =0;buy<=1;buy++){
+                
+                int profit = 0;
+                
+                if(buy){
+                //can buy
+            
+                    int x = -prices[index] + dp[index+1][0];  //buys          ,can't buy next
+                    int y =                + dp[index+1][1];  //does not buy  ,can buy next  
+            
+                profit = max(x,y);
+            
+                }else{
+                    //can sell only           
+            
+                    int x = prices[index] + dp[index+1][1];  //sells          ,can buy next
+                    int y =               + dp[index+1][0];  //does not sell  ,can't buy next 
+            
+                    profit = max(x,y);
+            
+                }     
+                
+                dp[index][buy] = profit;
+            }            
+        }
+        return dp[0][1];
     }
 };
+
+
+//RECURSIVE MEMOIZED SOLUTION
+
+// class Solution {
+// private:
+//     int helper(int index, int buy, vector<int> &prices,  vector<vector<int>> &dp){
+        
+//         if(index == prices.size()) return 0;
+        
+//         if(dp[index][buy] != -1) return dp[index][buy];
+        
+//         int profit = 0;
+//         if(buy){
+//             //can buy
+            
+//             int x = -prices[index] + helper(index+1, 0, prices, dp);  //buys          ,can't buy next
+//             int y =                + helper(index+1, 1, prices, dp);  //does not buy  ,can buy next
+            
+//             profit = max(x,y);
+            
+//         }else{
+//             //can sell only           
+            
+//             int x = prices[index] + helper(index+1, 1, prices, dp);   //sells          ,can buy next
+//             int y =               + helper(index+1, 0, prices, dp);  //does not sell  ,can't buy next 
+            
+//             profit = max(x,y);
+            
+//         }       
+//         return dp[index][buy] = profit;
+//     }
+// public:
+//     int maxProfit(vector<int>& prices) {
+        
+//         int size = prices.size();
+//         vector<vector<int>> dp(size, vector<int>(2, -1));
+//         return helper(0,1, prices , dp);        
+//     }
+// };
 
 
 
