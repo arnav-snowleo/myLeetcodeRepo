3c3f371598fd7a673aa01fe30675a71796235bd4
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-06-30 10:58:09 +0530

    Time: 707 ms (32.53%), Space: 207.1 MB (23.11%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,5 +1,67 @@
+// TABULATION SOLUTION
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3,0)));
+        
+        //WRITING base case is not needed as we initialised in declaration itself to 0 
+        
+//         // BASE CASE FOR (cap==0)
+//         for(int index=0;index<n;index++){
+            
+//             for(int buy=0;buy<2;buy++){
+                
+//                 dp[index][buy][0] = 0;
+//             }
+//         } 
+        
+//         // BASE CASE FOR (index==n)
+//         for(int cap=0;cap<3;cap++){
+            
+//             for(int buy=0;buy<2;buy++){
+                
+//                 dp[n][buy][cap] = 0;
+//             }
+//         } 
+
+        for(int index =n-1;index>=0;index--){
+            
+            for(int buy=0; buy<=1;buy++){
+                
+                for(int cap=1;cap<=2;cap++){  // CAP CAN START FROM 1, as for cap 0, ans is 0
+                    
+                    int profit = 0;
+                    if(buy){ // allowed to buy           
+                        int x = -prices[index] + dp[index+1][0][cap];    //buy
+                        int y =                + dp[index+1][1][cap];    //not buy
+                
+                        profit = max(x,y);    
+            
+                    }else{ //allowed to sell
+            
+                         int x = prices[index] + dp[index+1][1][cap-1];  //sell 
+                         int y =               + dp[index+1][0][cap];    //not sell
+                
+                         profit = max(x,y);  
+                    }             
+                    
+                    dp[index][buy][cap] = profit;
+                }
+            }
+        }
+        
+        return dp[0][1][2];
+    }
+};
+
+
+
 // MEMOIZED SOLUTION
 
+/*
 // TC : O(N*2*3)
 
 // index, buy, cap --three states --3d dp
@@ -38,3 +100,4 @@ public:
         return helper(0,1,prices,dp,2);
     }
 };
+*/
