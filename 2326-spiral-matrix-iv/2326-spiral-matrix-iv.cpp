cf09562e64a41428f195b97fe810445d62fddf74
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-07-04 01:29:44 +0530

    Time: 443 ms (33.33%), Space: 128 MB (33.33%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        
+        vector<vector<int>> matrix(m , vector<int>(n,-1));
+        
+        int minrow =0;
+        int mincol =0;
+        int maxrow =m-1;
+        int maxcol =n-1;
+        
+        ListNode* temp = head;
+        // if(temp == NULL) return matrix;
+        
+        //TRAVERSAL
+        while(temp!=NULL && minrow<=maxrow && mincol <=maxcol){
+            
+            //TOP WALL
+            for(int i=minrow;i<=maxcol;i++){
+                if(temp) matrix[minrow][i] = temp->val;
+                // cout<<temp->val<<" ";
+                if(temp) temp = temp->next;
+            }
+            minrow++;
+            
+            //RIGHT WALL
+            for(int i=minrow;i<=maxrow;i++){
+                if(temp) matrix[i][maxcol] = temp->val;
+                // cout<<temp->val<<" ";
+                if(temp) temp = temp->next;
+            }
+            maxcol--;
+            
+            //BOTTOM WALL
+            for(int i=maxcol;i>=mincol;i--){
+                if(temp) matrix[maxrow][i] = temp->val;
+                // cout<<temp->val<<" ";
+                if(temp) temp = temp->next;
+            }
+            maxrow--;
+            
+            //LEFT WALL
+            for(int i=maxrow;i>=minrow;i--){
+                if(temp) matrix[i][mincol] = temp->val; 
+                // cout<<temp->val<<" ";
+                if(temp) temp = temp->next;
+            }
+            mincol++;            
+        }        
+        return matrix;        
+    }
+};
