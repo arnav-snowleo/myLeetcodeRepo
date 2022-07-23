a30b0e5bff4178d01f5a8d068b36a94d3b24bf68
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-07-23 12:56:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (88.17%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        
+        int ans = 0;        
+        for(char ch: columnTitle){
+            
+            int x = ch - 'A' + 1;
+            ans = ans*26 + x;
+        }       
+        
+        return ans;   
+    }
+};
