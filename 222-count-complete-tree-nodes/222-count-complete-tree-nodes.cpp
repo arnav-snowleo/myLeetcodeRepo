0706a8608205d68ce694500d2296ea914a9391a6
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-07-29 10:01:10 +0530

    Time: 67 ms (18.96%), Space: 30.8 MB (62.85%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -10,6 +10,43 @@
  * };
  */
 
+// OPTIMISED: 0((logn)^2)
+class Solution {
+private:
+    int findHeight(TreeNode* node, bool left){
+        
+        int height =0;
+        
+        while(node){
+            height++;
+            if(left) node = node->left;
+            else node = node->right;            
+        }
+        
+        return height;                
+    }
+public:
+    int countNodes(TreeNode* root) {
+        
+        if(!root) return 0;
+        
+        int left_subtree_height = findHeight(root, true);
+        int right_subtree_height = findHeight(root, false);
+        
+        if(left_subtree_height == right_subtree_height) return (1<<left_subtree_height) - 1;                                                                         // 1<< right_subtree_height will aslo work
+        // this is 2 power height (i.e num of nodes in complete BT of height h)
+        
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);               
+    }
+};
+
+
+
+
+
+
+/*
 // BRUTE: 0(n) approach
 
 class Solution {
@@ -31,3 +68,4 @@ public:
         return ans(root) + 1;
     }
 };
+*/
