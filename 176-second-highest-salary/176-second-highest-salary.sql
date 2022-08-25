300ec26e170660f7f5c8e343779160fbec03454d
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2022-08-25 12:21:33 +0530

    Time: 374 ms (30.94%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,4 +1,15 @@
 # Write your MySQL query statement below
 
 
-select max(salary) as SecondHighestSalary from employee where salary < (select max(salary) from employee);
+# select max(salary) as SecondHighestSalary from employee where salary < (select max(salary) from employee);
+
+
+# USE DISTINCT KEYWORD else, this limit query does not include null values
+# select salary as SecondHighestSalary from employee order by salary desc LIMIT 1,1;
+
+select (
+
+    Select distinct salary  from employee order by salary desc limit 1,1
+)
+
+as SecondHighestSalary;
