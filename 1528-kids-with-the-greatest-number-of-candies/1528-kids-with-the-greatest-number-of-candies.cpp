48a08c18e7f65a95b473745e09a21a2d6280ad81
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2023-08-23 23:52:05 +0530

    Time: 4 ms (48.77%), Space: 8.9 MB (73.3%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
@@ -6,13 +6,10 @@ public:
         for(int i=0;i<size;i++)
             maxi = max(maxi, candies[i]);
 
-        vector<bool> res;
+        vector<bool> res(size,true);
         for(int i=0;i<size;i++){
-            if( candies[i]+extraCandies <maxi){
-                res.push_back(false);
-            }else{
-                res.push_back(true);
-            }
+            if( candies[i]+extraCandies <maxi)
+                res[i] = false;
         }
         return res;
     }
