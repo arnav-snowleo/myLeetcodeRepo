1923950214f2d39cb2824f6d0a1667e6cae46a08
Arnav Satrusal <68443456+arnav-snowleo@users.noreply.github.com>
2023-09-05 00:01:40 +0530

    Time: 4 ms (16.12%), Space: 6.4 MB (12.01%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
@@ -2,14 +2,23 @@ class Solution {
 public:
     int kthFactor(int n, int k) {
 
-        vector<int> factorList;
+        set<int> factorList;
         for(int i=1;i<=n;i++){
-            if(n%i==0)
-                factorList.emplace_back(i);
+            if(n%i==0){
+                factorList.insert(i);
+                factorList.insert(n/i);
+            }
         }
-        if(factorList.size()<k)
+        
+        vector<int> factors;
+        for(auto it: factorList)
+            factors.emplace_back(it);
+        
+        if(factors.size()<k)
             return -1;
-        return factorList[k-1];
+        return factors[k-1];
+
     }
 };
-//Brute O(n)
+// Little optimised -> TC: O(sqrt(n)) SC: O(numberOfFactors), 
+// using set, set stores in ascending order
